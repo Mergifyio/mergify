4ac4f5ba1e432cc50839324765d26203f6a72bf0
Mehdi Abaakouk <sileht@sileht.net>
2018-07-03 14:29:22 +0200

    Fix import typo

--- mergify_engine/web.py
+++ mergify_engine/web.py
@@ -43,7 +43,7 @@ app.config.from_object(rq_dashboard.default_settings)
 app.register_blueprint(rq_dashboard.blueprint, url_prefix="/rq")
 app.config["REDIS_URL"] = utils.get_redis_url()
 app.config["RQ_POLL_INTERVAL"] = 10000  # ms
-sentry = raven.contrb.flask.Sentry(app, dsn=config.SENTRY_URL)
+sentry = raven.contrib.flask.Sentry(app, dsn=config.SENTRY_URL)
 
 
 def get_queue():
