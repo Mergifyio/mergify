5894e6cffe01a24864118ca4780755f2e386199a
Mehdi Abaakouk <sileht@sileht.net>
2018-07-04 22:34:02 +0200

    deploy: Fix sentry quoting

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -28,33 +28,34 @@ echo "$PRODUCTION_KNOWN_HOSTS" | base64 -d > ~/.ssh/known_hosts
 chmod 600 ~/.ssh/id_ed25519
 
 # Create a new release
-curl https://sentry.io/api/0/organizations/${SENTRY_SLUG}/releases/ \
+curl -q https://sentry.io/api/0/organizations/${SENTRY_SLUG}/releases/ \
   -X POST \
-  -H "Authorization: Bearer ${SENTRY_SLUG}" \
+  -H "Authorization: Bearer ${SENTRY_TOKEN}" \
   -H "Content-Type: application/json" \
-  -d '
+  -d "
   {
-    "version": "2da95dfb052f477380608d59d32b4ab9",
-    "refs": [{
-        "repository":"mergifyio/merg",
-        "commit":"'${commit_id}'",
-        "previousCommit":"'${previous_commit_id}'"
+    \"version\": \"2da95dfb052f477380608d59d32b4ab9\",
+    \"refs\": [{
+        \"repository\":\"mergifyio/mergify-engine\",
+        \"commit\":\"${commit_id}\",
+        \"previousCommit\":\"${previous_commit_id}\"
     }],
-    "projects": ["mergify-engine"]
+    \"projects\": [\"mergify-engine\"]
 }
-'
+"
 
 # Create a new deploy
 git remote add production $PRODUCTION_URL
 git push production --force HEAD:master
-curl https://sentry.io/api/0/organizations/$SENTRY_SLUG/releases/${commit_id}/deploys/ \
+
+curl -q https://sentry.io/api/0/organizations/$SENTRY_SLUG/releases/${commit_id}/deploys/ \
   -X POST \
   -H "Authorization: Bearer ${SENTRY_TOKEN}" \
   -H "Content-Type: application/json" \
-  -d '
+  -d "
   {
-    "environment": "production",
-    "name": "'${commit_subject}'",
-    "url": "https://github.com/Mergifyio/mergify-engine/commit/'${commit_id}'"
+    \"environment\": \"production\",
+    \"name\": \"${commit_subject}\",
+    \"url\": \"https://github.com/Mergifyio/mergify-engine/commit/${commit_id}\"
 }
-'
+"
