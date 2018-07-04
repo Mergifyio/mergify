cc0d83181c538ec1db8b9f8aa0bf86fef4fdfe59
Mehdi Abaakouk <sileht@sileht.net>
2018-07-04 20:59:23 +0200

    Automate deployement in production

---
+++ tools/deploy.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+set -e
+
+error() {
+    echo "$1 unset, exiting"
+    exit 1
+}
+[ ! $PRODUCTION_URL ] && error PRODUCTION_URL
+[ ! $SENTRY_SLUG ] && error SENTRY_SLUG
+[ ! $SENTRY_TOKEN ] && error SENTRY_TOKEN
+
+commit_id="$(git rev-parse HEAD)"
+previous_commit_id="$(git rev-parse HEAD^)"
+commit_subject=$(git log -1 --pretty="%s")
+
+git remote add production $PRODUCTION_URL
+git push production --force
+
+# Create a new release
+curl https://sentry.io/api/0/organizations/${SENTRY_SLUG}/releases/ \
+  -X POST \
+  -H "Authorization: Bearer ${SENTRY_SLUG}" \
+  -H "Content-Type: application/json" \
+  -d '
+  {
+    "version": "2da95dfb052f477380608d59d32b4ab9",
+    "refs": [{
+        "repository":"mergifyio/merg",
+        "commit":"'${commit_id}'",
+        "previousCommit":"'${previous_commit_id}'"
+    }],
+    "projects": ["mergify-engine"]
+}
+'
+
+# Create a new deploy
+curl https://sentry.io/api/0/organizations/$SENTRY_SLUG/releases/${commit_id}/deploys/ \
+  -X POST \
+  -H "Authorization: Bearer ${SENTRY_TOKEN}" \
+  -H "Content-Type: application/json" \
+  -d '
+  {
+    "environment": "production",
+    "name": "'${commit_subject}'",
+    "url": "https://github.com/Mergifyio/mergify-engine/commit/'${commit_id}'"
+}
+'
