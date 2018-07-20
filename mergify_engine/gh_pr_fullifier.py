ac309e25504475320e638fa155ad0ab31a747e6e
Mehdi Abaakouk <sileht@sileht.net>
2018-07-20 10:49:48 +0200

    Don't retry when Github have a unexpected mergeable_state

--- mergify_engine/gh_pr_fullifier.py
+++ mergify_engine/gh_pr_fullifier.py
@@ -228,25 +228,8 @@ def compute_status(pull, **extra):
                 # refreshing the the pull. Or maybe this is a mergify bug :),
                 # so retry only 3 times
 
-                LOG.error("%s: the status is unexpected, requeue this event.",
-                          pull.pretty())
-                commit = pull.base.repo.get_commit(pull.head.sha)
-                status = commit.get_combined_status()
-                LOG.warning("reviews: %s", [r.raw_data for r in
-                                            list(pull.get_reviews())])
-                LOG.warning("status checks: %s", status.raw_data["statuses"])
-                LOG.warning("pull content: %s", pull.raw_data)
-
-                # NOTE(sileht): We have reviewers and the CI is OK, so this PR
-                # can be merged. But Github tell us it's blocked. As workaround
-                # we refresh our status_check, so Github should recompute the
-                # mergify_state and allow us to push the merge btn.
-                pull.mergify_engine_github_post_check_status(
-                    extra["redis"], extra["installation_id"],
-                    "success", "Will be merged soon")
-                time.sleep(1)
-                raise exceptions.RetryJob(3)
-
+                raise RuntimeError("%s: the mergeable_state is unexpected",
+                                   pull.pretty())
         elif (pull.mergify_engine["required_statuses"] ==
               StatusState.PENDING):
             # Maybe clean soon, or maybe this is the previous run selected PR
