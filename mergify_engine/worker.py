d10a85cbd680ebfba6168cddc44e3074c498c192
Mehdi Abaakouk <sileht@sileht.net>
2018-08-08 09:12:57 +0200

    Use isintance for comparing exc_type

--- mergify_engine/worker.py
+++ mergify_engine/worker.py
@@ -60,8 +60,8 @@ class MergifyWorker(rq.Worker):  # pragma: no cover
         self.push_exc_handler(self._retry_handler)
 
     def _retry_handler(self, job, exc_type, exc_value, traceback):
-        if ((exc_type == github.GithubException and exc_value.status >= 500) or
-            (exc_type == requests.exceptions.HTTPError and
+        if ((isinstance(exc_type, github.GithubException) and exc_value.status >= 500) or
+            (isinstance(exc_type, requests.exceptions.HTTPError) and
              exc_value.response.status_code >= 500)):
             backoff = datetime.timedelta(seconds=5)
 
