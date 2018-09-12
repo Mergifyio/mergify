ac38acca4db9295b5a0aa1440aca9ce06f8abe42
Mehdi Abaakouk <sileht@sileht.net>
2018-09-12 14:04:56 +0200

    tools: Change token to cleanup test accounts

--- tools/gh-accounts-cleaner.sh
+++ tools/gh-accounts-cleaner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-MERGIFYENGINE_MAIN_TOKEN=$(python3 -c 'import yaml ; print(yaml.load(open("test.yml").read())["MAIN_TOKEN"])')
-MERGIFYENGINE_FORK_TOKEN=$(python3 -c 'import yaml ; print(yaml.load(open("test.yml").read())["FORK_TOKEN"])')
+MERGIFYENGINE_MAIN_TOKEN=$(python3 -c 'import yaml ; print(yaml.load(open("test.yml").read())["MAIN_TOKEN_DELETE"])')
+MERGIFYENGINE_FORK_TOKEN=$(python3 -c 'import yaml ; print(yaml.load(open("test.yml").read())["FORK_TOKEN_DELETE"])')
 for token in $MERGIFYENGINE_MAIN_TOKEN $MERGIFYENGINE_FORK_TOKEN ; do
     for i in $(curl -qq -H "Authorization: token $token" https://api.github.com/user/repos 2>/dev/null | jq -r '.[].full_name'); do
         echo $i
