3fc1d9b9b23ec1dee6c208f2a6e77c2ca645ad41
Mehdi Abaakouk <sileht@sileht.net>
2019-02-08 11:43:45 +0100

    Deploy to new infra

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,61 +1,28 @@
 #!/bin/bash
-set -e
 
 error() {
     echo "$1 unset, exiting"
     exit 1
 }
 [ -z "$PRODUCTION_PORT" ] && error PRODUCTION_PORT
-[ -z "$PRODUCTION_URL" ] && error PRODUCTION_URL
+[ -z "$PRODUCTION_HOST" ] && error PRODUCTION_HOST
 [ -z "$PRODUCTION_KEY" ] && error PRODUCTION_KEY
 [ -z "$PRODUCTION_KNOWN_HOSTS" ] && error PRODUCTION_KNOWN_HOSTS
-[ -z "$SENTRY_SLUG" ] && error SENTRY_SLUG
-[ -z "$SENTRY_TOKEN" ] && error SENTRY_TOKEN
-
-commit_id="$(git rev-parse HEAD)"
-previous_commit_id="$(git rev-parse HEAD^)"
-commit_subject=$(git log -1 --pretty="%s")
 
 # ssh port looks not working with git version of travis
 mkdir -p ~/.ssh
-cat > ~/.ssh/config <<EOF
-Host *
-  Port $PRODUCTION_PORT
-EOF
 
 echo "$PRODUCTION_KEY" | base64 -d > ~/.ssh/id_ed25519
 echo "$PRODUCTION_KNOWN_HOSTS" | base64 -d > ~/.ssh/known_hosts
 chmod 600 ~/.ssh/id_ed25519
 
-# Create a new release
-curl -q https://sentry.io/api/0/organizations/${SENTRY_SLUG}/releases/ \
-  -X POST \
-  -H "Authorization: Bearer ${SENTRY_TOKEN}" \
-  -H "Content-Type: application/json" \
-  -d "
-  {
-    \"version\": \"2da95dfb052f477380608d59d32b4ab9\",
-    \"refs\": [{
-        \"repository\":\"mergifyio/mergify-engine\",
-        \"commit\":\"${commit_id}\",
-        \"previousCommit\":\"${previous_commit_id}\"
-    }],
-    \"projects\": [\"mergify-engine\"]
-}
-"
+docker tag mergify-engine-dev mergifyio/engine:latest
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+docker push mergifyio/engine:latest
 
-# Create a new deploy
-git remote add production $PRODUCTION_URL
-git push production --force HEAD:master
-
-curl -q https://sentry.io/api/0/organizations/$SENTRY_SLUG/releases/${commit_id}/deploys/ \
-  -X POST \
-  -H "Authorization: Bearer ${SENTRY_TOKEN}" \
-  -H "Content-Type: application/json" \
-  -d "
-  {
-    \"environment\": \"production\",
-    \"name\": \"${commit_subject}\",
-    \"url\": \"https://github.com/Mergifyio/mergify-engine/commit/${commit_id}\"
-}
+ssh $PRODUCTION_HOST -p $PRODUCTION_PORT bash -c "
+set -ex;
+cd mergify-engine-docker;
+ENV=prod docker-compose -f docker-compose.yaml pull engine;
+ENV=prod docker-compose -f docker-compose.yaml up -d;
 "
