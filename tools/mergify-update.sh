e8ce254cdc6278deb17008b9756105b478a6cb45
Mehdi Abaakouk <sileht@sileht.net>
2019-04-24 15:07:33 +0200

    strict: set to zset upgrade removal
    The upgrade is done, we can remove the script

--- tools/mergify-update.sh
+++ tools/mergify-update.sh
@@ -10,25 +10,6 @@ cd mergify-engine-docker
 export ENV=prod
 
 docker-compose -f docker-compose.yaml pull engine
-
-# NOTE(sileht): Upgrade queue set to zset
-# TODO(
-docker-compose -f docker-compose.yaml stop engine-beat engine
-cat > upgrade.lua << EOF
-local queues = redis.call("keys", "strict-merge-queues~*")
-for idx, queue in ipairs(queues) do
-    if redis.call("type", queue) ~= "zset" then
-        local oldqueue = "deprecated-set-" .. queue
-        redis.call("RENAME", queue, oldqueue)
-        redis.call("ZUNIONSTORE", queue, 1, oldqueue)
-    end
-end
-EOF
-docker cp upgrade.lua mergify_celery_1:/tmp/
-docker exec mergify_celery_1 redis-cli -h storage --eval /tmp/upgrade.lua
-docker exec mergify_celery_1 rm -f /tmp/upgrade.lua
-rm -f upgrade.lua
-
 docker-compose -f docker-compose.yaml up -d
 docker restart mergify_nginx_1
 
