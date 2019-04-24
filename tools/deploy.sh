e5ab26dd27635aff20f0ad84a645c9b25bae97c7
Mehdi Abaakouk <sileht@sileht.net>
2019-04-24 14:58:03 +0200

    strict: upgrade set to zset script

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -22,12 +22,4 @@ docker tag mergify-engine-dev mergifyio/engine:latest
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 docker push mergifyio/engine:latest
 
-# TODO(sileht): nginx cache the ip of the engine container, when the
-# engine container is recreated, nginx still try to connect to the old one
-# So we temporary restart the nginx container each time we deploy mergify
-ssh $PRODUCTION_HOST -p $PRODUCTION_PORT bash -c "set -euxo pipefail;
-cd mergify-engine-docker;
-ENV=prod docker-compose -f docker-compose.yaml pull engine;
-ENV=prod docker-compose -f docker-compose.yaml up -d;
-docker restart mergify_nginx_1;
-"
+cat tools/mergify-update.sh | ssh -p $PRODUCTION_PORT $PRODUCTION_HOST bash
---
+++ tools/mergify-update.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+# TODO(sileht): nginx cache the ip of the engine container, when the
+# engine container is recreated, nginx still try to connect to the old one
+# So we temporary restart the nginx container each time we deploy mergify
+
+cd mergify-engine-docker
+export ENV=prod
+
+docker-compose -f docker-compose.yaml pull engine
+
+# NOTE(sileht): Upgrade queue set to zset
+# TODO(
+docker-compose -f docker-compose.yaml stop engine-beat engine
+cat > upgrade.lua << EOF
+local queues = redis.call("keys", "strict-merge-queues~*")
+for idx, queue in ipairs(queues) do
+    if redis.call("type", queue) ~= "zset" then
+        local oldqueue = "deprecated-set-" .. queue
+        redis.call("RENAME", queue, oldqueue)
+        redis.call("ZUNIONSTORE", queue, 1, oldqueue)
+    end
+end
+EOF
+docker cp upgrade.lua mergify_celery_1:/tmp/
+docker exec -it mergify_celery_1 redis-cli -h storage --eval /tmp/upgrade.lua
+docker exec -it mergify_celery_1 rm -f /tmp/upgrade.lua
+rm -f upgrade.lua
+
+docker-compose -f docker-compose.yaml up -d
+docker restart mergify_nginx_1
+
