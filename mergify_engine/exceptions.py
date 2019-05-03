ec689747144322e9e8d47150595b9542e7d36409
Mehdi Abaakouk <sileht@sileht.net>
2019-05-03 16:01:16 +0200

    requests: use more generic connection issue
    This will also catch all Timeouts and TooManyRedirects related error

--- mergify_engine/exceptions.py
+++ mergify_engine/exceptions.py
@@ -28,7 +28,7 @@ def need_retry(exception):
            exception.status >= 500) or
           (isinstance(exception, requests.exceptions.HTTPError) and
            exception.response.status_code >= 500) or
-          isinstance(exception, requests.exceptions.ConnectionError)):
+          isinstance(exception, requests.exceptions.RequestException)):
         return 30
 
     # NOTE(sileht): Most of the times token are just temporary invalid, Why ?
