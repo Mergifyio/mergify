bc7d00390c7eb2065a2bc66dd212d33410c32010
Julien Danjou <julien@danjou.info>
2019-12-11 19:26:00 +0100

    datadog: always monitor Redis
    If the web dyno is ever down (for a long time) this means we'll lose monitoring
    for a while.
    
    It shouldn't be a problem to monitor with all dynos for this part.

--- datadog/prerun.sh
+++ datadog/prerun.sh
@@ -32,27 +32,21 @@ EOF
 esac
 
 
-# Only the first web dyno will monitor redis
-if [ "$DYNO" == "web.1" ]; then
-    REDIS_REGEX='^redis://([^:]+):([^@]+)@([^:]+):([^/]+)$'
+REDIS_REGEX='^redis://([^:]+):([^@]+)@([^:]+):([^/]+)$'
 
-    if [ -n "$MERGIFYENGINE_STORAGE_URL" ]; then
-        if [[ $MERGIFYENGINE_STORAGE_URL =~ $REDIS_REGEX ]]; then
-        sed -i "s/<CACHE HOST>/${BASH_REMATCH[3]}/" "$DD_CONF_DIR/conf.d/redisdb.d/conf.yaml"
-        sed -i "s/<CACHE PASSWORD>/${BASH_REMATCH[2]}/" "$DD_CONF_DIR/conf.d/redisdb.d/conf.yaml"
-        sed -i "s/<CACHE PORT>/${BASH_REMATCH[4]}/" "$DD_CONF_DIR/conf.d/redisdb.d/conf.yaml"
-        fi
+if [ -n "$MERGIFYENGINE_STORAGE_URL" ]; then
+    if [[ $MERGIFYENGINE_STORAGE_URL =~ $REDIS_REGEX ]]; then
+    sed -i "s/<CACHE HOST>/${BASH_REMATCH[3]}/" "$DD_CONF_DIR/conf.d/redisdb.d/conf.yaml"
+    sed -i "s/<CACHE PASSWORD>/${BASH_REMATCH[2]}/" "$DD_CONF_DIR/conf.d/redisdb.d/conf.yaml"
+    sed -i "s/<CACHE PORT>/${BASH_REMATCH[4]}/" "$DD_CONF_DIR/conf.d/redisdb.d/conf.yaml"
     fi
+fi
 
 
-    if [ -n "$MERGIFYENGINE_CELERY_BROKER_URL" ]; then
-        if [[ $MERGIFYENGINE_CELERY_BROKER_URL =~ $REDIS_REGEX ]]; then
-            sed -i "s/<CELERY HOST>/${BASH_REMATCH[3]}/" "$DD_CONF_DIR/conf.d/redisdb.d/conf.yaml"
-            sed -i "s/<CELERY PASSWORD>/${BASH_REMATCH[2]}/" "$DD_CONF_DIR/conf.d/redisdb.d/conf.yaml"
-            sed -i "s/<CELERY PORT>/${BASH_REMATCH[4]}/" "$DD_CONF_DIR/conf.d/redisdb.d/conf.yaml"
-        fi
+if [ -n "$MERGIFYENGINE_CELERY_BROKER_URL" ]; then
+    if [[ $MERGIFYENGINE_CELERY_BROKER_URL =~ $REDIS_REGEX ]]; then
+        sed -i "s/<CELERY HOST>/${BASH_REMATCH[3]}/" "$DD_CONF_DIR/conf.d/redisdb.d/conf.yaml"
+        sed -i "s/<CELERY PASSWORD>/${BASH_REMATCH[2]}/" "$DD_CONF_DIR/conf.d/redisdb.d/conf.yaml"
+        sed -i "s/<CELERY PORT>/${BASH_REMATCH[4]}/" "$DD_CONF_DIR/conf.d/redisdb.d/conf.yaml"
     fi
-
-else
-    rm -f $DD_CONF_DIR/conf.d/redisdb.d/conf.yaml
 fi
