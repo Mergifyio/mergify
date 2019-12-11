82887c5ab2eb8f444ee241cb068006fa2e3a23d5
Julien Danjou <julien@danjou.info>
2019-12-11 18:50:35 +0100

    datadog: generate process conf based on DYNOTYPE

--- datadog/prerun.sh
+++ datadog/prerun.sh
@@ -1,9 +1,36 @@
 #!/usr/bin/env bash
 
-# Disable the Datadog Agent based on dyno type
-if [ "$DYNOTYPE" == "run" ]; then
-  DISABLE_DATADOG_AGENT="true"
-fi
+case $DYNOTYPE in
+    run)
+        DISABLE_DATADOG_AGENT="true"
+        ;;
+    web)
+        cat >> "$DD_CONF_DIR/conf.d/process.d/conf.yaml" <<EOF
+  - name: gunicorn-worker
+    search_string: ['^gunicorn: worker']
+    exact_match: false
+  - name: gunicorn-master
+    search_string: ['^gunicorn: master']
+    exact_match: false
+EOF
+        ;;
+    engine)
+        cat >> "$DD_CONF_DIR/conf.d/process.d/conf.yaml" <<EOF
+  - name: celery-main
+    # [celeryd: celery@aeade076-e94d-452f-8af0-ad8d5850fa4c:MainProcess] -active- (worker --beat --app mergifyio.synchronizator --concurrency 4 --queues schedule,github.accounts,github.events,celery)
+    search_string: ['\[celeryd: .+:MainProcess\]']
+    exact_match: false
+  - name: celery-worker
+    # [celeryd: celery@aeade076-e94d-452f-8af0-ad8d5850fa4c:ForkPoolWorker-2]
+    search_string: ['\[celeryd: .+:ForkPoolWorker']
+    exact_match: false
+  - name: celery-beat
+    # celery-beat
+    search_string: ['[celery beat]']
+EOF
+        ;;
+esac
+
 
 # Only the first web dyno will monitor redis
 if [ "$DYNO" == "web.1" ]; then
---
+++ datadog/validate.py
@@ -0,0 +1,7 @@
+import glob
+
+import yaml
+
+for fname in glob.glob("datadog/conf.d/*.yaml"):
+    with open(fname) as f:
+        yaml.safe_load(f)
