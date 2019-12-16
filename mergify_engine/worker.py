7388881496d84bee43fdb096fadf283f158a838e
Mehdi Abaakouk <sileht@sileht.net>
2019-12-16 13:06:54 +0100

    celery: increase tasks timetout
    Hosting tmp dir can be slow, add some times for git stuffs.

--- mergify_engine/worker.py
+++ mergify_engine/worker.py
@@ -44,8 +44,8 @@ app.conf.task_routes = ([("mergify_engine.tasks.*", {"queue": "mergify"})],)
 app.conf.task_soft_time_limit = 1 * 60
 app.conf.task_time_limit = 2 * 60
 # FIXME(sileht): Backport is using get_pulls() that always returns ton of PRs.
-app.conf.task_soft_time_limit = 5 * 60
-app.conf.task_time_limit = 10 * 60
+app.conf.task_soft_time_limit = 15 * 60
+app.conf.task_time_limit = 16 * 60
 
 
 @signals.setup_logging.connect
