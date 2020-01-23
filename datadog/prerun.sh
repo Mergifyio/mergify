0141de3f6c9e290bf1eef0b9268039bcf55cec1a
Mehdi Abaakouk <sileht@sileht.net>
2020-01-23 11:58:42 +0100

    fix(datadog): fixup ps:exec

--- datadog/prerun.sh
+++ datadog/prerun.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
+# If we are in ps:exec do nothing plz
+# see https://github.com/DataDog/heroku-buildpack-datadog/issues/155
+if [ -z "$DYNO" ]; then
+    DISABLE_DATADOG_AGENT="true"
+    exit 0
+fi
+
 case $DYNOTYPE in
     run)
         DISABLE_DATADOG_AGENT="true"
+        exit 0
         ;;
     web)
         cat > "$DATADOG_CONF" <<EOF
@@ -67,29 +75,28 @@ EOF
         ;;
 esac
 
-# If we are in ps:exec do nothing plz
-# see https://github.com/DataDog/heroku-buildpack-datadog/issues/155
-if [ -n "$DYNO" ]; then
-    # Workaround for https://github.com/DataDog/heroku-buildpack-datadog/issues/155
-    # When datadog.sh is called it will copy the example and overwrite our conf
-    cp "$DATADOG_CONF" "$DATADOG_CONF.example"
+REDIS_REGEX='^redis://([^:]+):([^@]+)@([^:]+):([^/]+)$'
 
-    REDIS_REGEX='^redis://([^:]+):([^@]+)@([^:]+):([^/]+)$'
-
-    if [ -n "$MERGIFYENGINE_STORAGE_URL" ]; then
-        if [[ $MERGIFYENGINE_STORAGE_URL =~ $REDIS_REGEX ]]; then
-            sed -i "s/<CACHE HOST>/${BASH_REMATCH[3]}/" "$DD_CONF_DIR/conf.d/redisdb.d/conf.yaml"
-            sed -i "s/<CACHE PASSWORD>/${BASH_REMATCH[2]}/" "$DD_CONF_DIR/conf.d/redisdb.d/conf.yaml"
-            sed -i "s/<CACHE PORT>/${BASH_REMATCH[4]}/" "$DD_CONF_DIR/conf.d/redisdb.d/conf.yaml"
-        fi
-    fi
-
-
-    if [ -n "$MERGIFYENGINE_CELERY_BROKER_URL" ]; then
-        if [[ $MERGIFYENGINE_CELERY_BROKER_URL =~ $REDIS_REGEX ]]; then
-            sed -i "s/<CELERY HOST>/${BASH_REMATCH[3]}/" "$DD_CONF_DIR/conf.d/redisdb.d/conf.yaml"
-            sed -i "s/<CELERY PASSWORD>/${BASH_REMATCH[2]}/" "$DD_CONF_DIR/conf.d/redisdb.d/conf.yaml"
-            sed -i "s/<CELERY PORT>/${BASH_REMATCH[4]}/" "$DD_CONF_DIR/conf.d/redisdb.d/conf.yaml"
-        fi
+if [ -n "$MERGIFYENGINE_STORAGE_URL" ]; then
+    if [[ $MERGIFYENGINE_STORAGE_URL =~ $REDIS_REGEX ]]; then
+        sed -i "s/<CACHE HOST>/${BASH_REMATCH[3]}/" "$DD_CONF_DIR/conf.d/redisdb.d/conf.yaml"
+        sed -i "s/<CACHE PASSWORD>/${BASH_REMATCH[2]}/" "$DD_CONF_DIR/conf.d/redisdb.d/conf.yaml"
+        sed -i "s/<CACHE PORT>/${BASH_REMATCH[4]}/" "$DD_CONF_DIR/conf.d/redisdb.d/conf.yaml"
     fi
 fi
+
+
+if [ -n "$MERGIFYENGINE_CELERY_BROKER_URL" ]; then
+    if [[ $MERGIFYENGINE_CELERY_BROKER_URL =~ $REDIS_REGEX ]]; then
+        sed -i "s/<CELERY HOST>/${BASH_REMATCH[3]}/" "$DD_CONF_DIR/conf.d/redisdb.d/conf.yaml"
+        sed -i "s/<CELERY PASSWORD>/${BASH_REMATCH[2]}/" "$DD_CONF_DIR/conf.d/redisdb.d/conf.yaml"
+        sed -i "s/<CELERY PORT>/${BASH_REMATCH[4]}/" "$DD_CONF_DIR/conf.d/redisdb.d/conf.yaml"
+    fi
+fi
+
+# Workaround for https://github.com/DataDog/heroku-buildpack-datadog/issues/155
+# When datadog.sh is called it will copy the example and project confd, and
+# overwrite our conf
+cp -f "$DATADOG_CONF" "$DATADOG_CONF.example"
+cp -f "$DD_CONF_DIR/conf.d/process.d/conf.yaml" "$APP_DATADOG_CONF_DIR/process.yml"
+cp -f "$DD_CONF_DIR/conf.d/redisdb.d/conf.yaml" "$APP_DATADOG_CONF_DIR/redis.yml"
