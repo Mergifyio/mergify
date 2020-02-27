c403f4c66eacd2ff4b84170f317fd38f9724a842
Mehdi Abaakouk <sileht@sileht.net>
2020-02-27 17:17:05 +0100

    fix: always return report with assign action

--- mergify_engine/actions/assign.py
+++ mergify_engine/actions/assign.py
@@ -6,5 +6,8 @@ from mergify_engine import actions
 class AssignAction(actions.Action):
     validator = {voluptuous.Required("users", default=[]): [str]}
 
+    silent_report = True
+
     def run(self, pull, sources, missing_conditions):
         pull.g_pull.as_issue().add_to_assignees(*self.config["users"])
+        return ("success", "Assignees added", ", ".join(self.config["users"]))
