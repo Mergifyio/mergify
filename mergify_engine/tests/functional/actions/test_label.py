18da22974cb98ed6819ae4a1e3c8822e3b35c038
Julien Danjou <julien@danjou.info>
2020-03-19 16:07:13 +0100

    feat(actions/label): add remove_all option
    This new option allows to remove all labels of a pull request.
    
    Relates to #360

---
+++ mergify_engine/tests/functional/actions/test_label.py
@@ -0,0 +1,42 @@
+# -*- encoding: utf-8 -*-
+#
+# Copyright © 2020 Julien Danjou <jd@mergify.io>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+import yaml
+
+from mergify_engine.tests.functional import base
+
+
+class TestLabelAction(base.FunctionalTestBase):
+    def test_label_remove_all(self):
+        rules = {
+            "pull_request_rules": [
+                {
+                    "name": "delete all labels",
+                    "conditions": ["base=master", "label=stable"],
+                    "actions": {"label": {"remove_all": True,}},
+                }
+            ]
+        }
+
+        self.setup_repo(yaml.dump(rules))
+
+        p, _ = self.create_pr()
+        self.add_label(p, "stable")
+
+        pulls = list(self.r_o_admin.get_pulls())
+        self.assertEqual(1, len(pulls))
+        self.assertEqual(
+            [], pulls[0].labels,
+        )
