08baae89dfc01b7f999072eab8aa494205e24ba1
Mehdi Abaakouk <sileht@sileht.net>
2020-03-21 10:16:58 +0100

    feat(action/assign): use httpx only

--- mergify_engine/actions/assign.py
+++ mergify_engine/actions/assign.py
@@ -1,3 +1,20 @@
+# -*- encoding: utf-8 -*-
+#
+#  Copyright © 2020 Mehdi Abaakouk <sileht@mergify.io>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import httpx
 import voluptuous
 
 from mergify_engine import actions
@@ -9,5 +26,19 @@ class AssignAction(actions.Action):
     silent_report = True
 
     def run(self, pull, sources, missing_conditions):
-        pull.g_pull.as_issue().add_to_assignees(*self.config["users"])
+        wanted = set(self.config["users"])
+        already = set((user["login"] for user in pull.data["assignees"]))
+        assignees = list(wanted - already)
+        try:
+            pull.client.post(
+                f"issues/{pull.data['number']}/assignees",
+                json={"assignees": assignees},
+            )
+        except httpx.HTTPClientSideError as e:  # pragma: no cover
+            return (
+                None,
+                "Unable to add assignees",
+                f"GitHub error: [{e.status_code}] `{e.message}`",
+            )
+
         return ("success", "Assignees added", ", ".join(self.config["users"]))
