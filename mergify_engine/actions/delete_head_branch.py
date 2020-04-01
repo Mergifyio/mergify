Mergifyio/mergify

commit feca30e648dbd476c70452f01dbe2d759fd13249

feat(action/copy): use httpx only

mergify_engine/actions/delete_head_branch.py
@@ -16,7 +16,7 @@
 
 from urllib import parse
 
-import github
+import httpx
 import voluptuous
 
 from mergify_engine import actions
@@ -32,46 +32,41 @@ class DeleteHeadBranchAction(actions.Action):
         if pull.from_fork:
             return ("success", "Pull request come from fork", "")
 
-        if pull.state == "closed":
+        if pull.data["state"] == "closed":
             if self.config is None or not self.config["force"]:
                 pulls_using_this_branch = list(
-                    pull.g_pull.base.repo.get_pulls(base=pull.head_ref)
+                    pull.client.items("pulls", json={"base": pull.data["head"]["ref"]})
                 )
                 if pulls_using_this_branch:
                     return (
                         "success",
                         "Branch `{}` was not deleted "
                         "because it is used by {}".format(
-                            pull.head_ref,
-                            " ".join("#%d" % p.number for p in pulls_using_this_branch),
+                            pull.data["head"]["ref"],
+                            " ".join(
+                                "#%d" % p["number"] for p in pulls_using_this_branch
+                            ),
                         ),
                         "",
                     )
 
+            ref_to_delete = parse.quote(pull.data["head"]["ref"], safe="")
             try:
-                pull.g_pull.head.repo._requester.requestJsonAndCheck(
-                    "DELETE",
-                    pull.g_pull.base.repo.url
-                    + "/git/refs/heads/"
-                    + parse.quote(pull.head_ref, safe=""),
-                )
-            except github.GithubException as e:
-                if e.status >= 500:
-                    raise
-                elif e.status not in [422, 404]:
+                pull.client.delete(f"git/refs/heads/{ref_to_delete}")
+            except httpx.HTTPClientSideError as e:
+                if e.status_code not in [422, 404]:
                     return (
                         "failure",
                         "Unable to delete the head branch",
-                        f"GitHub error: [{e.status}] `{e.data['message']}`",
+                        f"GitHub error: [{e.status_code}] `{e.message}`",
                     )
             return (
                 "success",
-                "Branch `%s` has been deleted" % pull.head_ref,
+                f"Branch `{pull.data['head']['ref']}` has been deleted",
                 "",
             )
         return (
             None,
-            "Branch `%s` will be deleted once the pull request is closed"
-            % pull.head_ref,
+            f"Branch `{pull.data['head']['ref']}` will be deleted once the pull request is closed",
             "",
         )

zfixtures/cassettes/TestEngineV2Scenario/test_backport_conflicts/reponame
@@ -1,1 +1,1 @@
-d097a5a0-4672-4d27-a165-872f4bc7a90b
+c11289f4-76ac-4cd8-82cd-aceca76b945c

zfixtures/cassettes/TestEngineV2Scenario/test_backport_ignore_conflicts/reponame
@@ -1,1 +1,1 @@
-0a415e0a-d8cc-403e-91c0-1bc2474f01ad
+32eea0b0-de00-4fb0-9e3e-920648c0e33f

zfixtures/cassettes/TestEngineV2Scenario/test_backport_merge_commit/reponame
@@ -1,1 +1,1 @@
-74d5edea-1b8d-4faf-8aef-eee5efe4ef8b
+2642bb02-92bc-485f-ae33-b9445a8378dc

zfixtures/cassettes/TestEngineV2Scenario/test_backport_merge_commit_regexes/reponame
@@ -1,1 +1,1 @@
-40804c73-3d04-46da-a244-9559332bcbc6
+5be13794-e018-4687-9dcf-21f53d1cab6e

zfixtures/cassettes/TestEngineV2Scenario/test_backport_rebase_and_merge/reponame
@@ -1,1 +1,1 @@
-e9fcc815-ee8b-4b62-b40a-37efb503f105
+4d616fd1-6024-4a97-9363-3eea102ee6fa

zfixtures/cassettes/TestEngineV2Scenario/test_backport_squash_and_merge/reponame
@@ -1,1 +1,1 @@
-65fe99da-4a8c-4c11-96fc-97d163087239
+644221a1-7afe-4c38-86c4-dcea712806af

zfixtures/cassettes/TestEngineV2Scenario/test_command_backport/reponame
@@ -1,1 +1,1 @@
-6da77af8-d161-4326-8920-95e71786dc53
+35d1a8ef-23ba-40f3-b777-4c86868c4cfc

commit b1233afe2a70263803c8f506fdef7378db838b01

fix(delete_head_branch): correctly list base branch
Commit feca30e648dbd476c70452f01dbe2d759fd13249 introduced a regression in the
way the existing pull requests are listed. It would list *all* PRs without
filtering, preventing the branch to be deleted.

This modifies the existing test so it catches that mistake, and fixes the issue
by passing the parameter correctly to the URL.

mergify_engine/actions/delete_head_branch.py
@@ -35,7 +35,7 @@ class DeleteHeadBranchAction(actions.Action):
         if ctxt.pull["state"] == "closed":
             if self.config is None or not self.config["force"]:
                 pulls_using_this_branch = list(
-                    ctxt.client.items("pulls", json={"base": ctxt.pull["head"]["ref"]})
+                    ctxt.client.items("pulls", base=ctxt.pull["head"]["ref"])
                 )
                 if pulls_using_this_branch:
                     return (

zfixtures/cassettes/TestDeleteHeadBranchAction/test_delete_branch/reponame
@@ -1,1 +1,1 @@
-70cb5862-18c3-4196-9dba-dd69abbd7925
+506f97a2-47fd-48d7-9ea1-d18de09d306a

zfixtures/cassettes/TestDeleteHeadBranchAction/test_delete_branch_with_dep_force/reponame
@@ -1,1 +1,1 @@
-3f919204-efbc-41ab-bf9b-e4bba6fcd0b4
+e55d9a63-d361-4f63-b7ae-68bda2982afa

zfixtures/cassettes/TestDeleteHeadBranchAction/test_delete_branch_with_dep_no_force/reponame
@@ -1,1 +1,1 @@
-364a566f-3352-46ac-bf58-33df0135ce1a
+b804fffd-9d45-4fcf-a271-fc3fe20b99a2