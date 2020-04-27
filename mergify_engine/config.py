a13c6660d6fb7402f855c6cdfb6f549abf0ef00f
Mehdi Abaakouk <sileht@sileht.net>
2020-04-27 17:53:20 +0200

    feat: remove ab testing for streams

--- mergify_engine/config.py
+++ mergify_engine/config.py
@@ -82,12 +82,6 @@ Schema = voluptuous.Schema(
         voluptuous.Required("STORAGE_URL", default="redis://localhost:6379?db=8"): str,
         voluptuous.Required("STREAM_URL", default="redis://localhost:6379?db=7"): str,
         voluptuous.Required("STREAM_WORKERS", default=7): voluptuous.Coerce(int),
-        voluptuous.Required(
-            "AB_TESTING_INSTALLATION_IDS", default="499592"
-        ): voluptuous.Any(None, CommaSeparatedIntList),
-        voluptuous.Required("AB_TESTING_PERCENTAGE", default=1): voluptuous.Any(
-            None, voluptuous.Coerce(int)
-        ),
         voluptuous.Required("CACHE_TOKEN_SECRET"): str,
         voluptuous.Required(
             "CELERY_BROKER_URL", default="redis://localhost:6379/9"
