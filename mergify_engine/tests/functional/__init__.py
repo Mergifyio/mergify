2029907039e885fe5d58eccf53af0c9b004af71f
Mehdi Abaakouk <sileht@sileht.net>
2020-05-27 23:27:56 +0200

    feat: handle AsyncClient retries
    This reverts commit eb573646074fe8b6083b6dda1d10488ae53a8cfd.

--- mergify_engine/tests/functional/__init__.py
+++ mergify_engine/tests/functional/__init__.py
@@ -1,16 +1,37 @@
 import requests
+import urllib3
 
 from mergify_engine import config
-from mergify_engine.clients import http
 
 
 real_session_init = requests.sessions.Session.__init__
+
+RETRY = urllib3.Retry(
+    total=None,
+    redirect=3,
+    connect=5,
+    read=5,
+    status=5,
+    backoff_factor=0.2,
+    status_forcelist=list(range(500, 599)) + [429],
+    method_whitelist=[
+        "HEAD",
+        "TRACE",
+        "GET",
+        "PUT",
+        "OPTIONS",
+        "DELETE",
+        "POST",
+        "PATCH",
+    ],
+    raise_on_status=False,
+)
 
 
 def retring_session_init(self, *args, **kwargs):
     real_session_init(self, *args, **kwargs)
 
-    adapter = requests.adapters.HTTPAdapter(max_retries=http.RETRY)
+    adapter = requests.adapters.HTTPAdapter(max_retries=RETRY)
 
     self.mount(config.GITHUB_API_URL, adapter)
 
