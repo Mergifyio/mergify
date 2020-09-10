cbfe747e422c8a3c1ca1d643831ee190ce16946d
Mehdi ABAAKOUK <sileht@sileht.net>
2020-09-10 20:25:00 +0200

    Revert "feat: don't hidden rules with unmatched title/body/files"
    This reverts commit f41de0a654f8b211637b77a0918175835e50a1d1.

--- mergify_engine/tests/unit/actions/test_merge.py
+++ mergify_engine/tests/unit/actions/test_merge.py
@@ -26,19 +26,12 @@ PR = {
     "number": 43,
     "state": "unknown",
     "mergeable_state": "ok",
-    "merged_by": {"login": "me"},
+    "merged_by": "me",
     "merged": False,
     "merged_at": None,
     "title": "My PR title",
     "user": {"login": "jd"},
-    "head": {"ref": "fork", "sha": "shasha"},
-    "base": {"ref": "master"},
-    "assignees": [],
-    "locked": False,
-    "labels": [],
-    "requested_reviewers": [],
-    "requested_teams": [],
-    "milestone": None,
+    "head": {"sha": "shasha"},
 }
 
 
@@ -138,7 +131,7 @@ my title
 def test_merge_commit_message(body, title, message, mode):
     pull = PR.copy()
     pull["body"] = body
-    client = mock.MagicMock()
+    client = mock.Mock()
     ctxt = context.Context(client=client, pull=pull, subscription={})
     ctxt.checks = {"my CI": "success"}
     pr = ctxt.pull_request
@@ -171,9 +164,7 @@ on two lines"""
 def test_merge_commit_message_undefined(body):
     pull = PR.copy()
     pull["body"] = body
-    pr = context.Context(
-        client=mock.MagicMock(), pull=pull, subscription={}
-    ).pull_request
+    pr = context.Context(client=mock.Mock(), pull=pull, subscription={}).pull_request
     with pytest.raises(context.RenderTemplateFailure) as x:
         action.MergeAction._get_commit_message(pr)
         assert str(x) == "foobar"
@@ -197,9 +188,7 @@ here is my message {{ and broken template
 def test_merge_commit_message_syntax_error(body, error):
     pull = PR.copy()
     pull["body"] = body
-    pr = context.Context(
-        client=mock.MagicMock(), pull=pull, subscription={}
-    ).pull_request
+    pr = context.Context(client=mock.Mock(), pull=pull, subscription={}).pull_request
     with pytest.raises(context.RenderTemplateFailure) as rmf:
         action.MergeAction._get_commit_message(pr)
         assert str(rmf) == error
