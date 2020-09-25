e43e9f4e94f562b88427c478d9f1883d493bd0b4
Julien Danjou <julien@danjou.info>
2020-09-25 19:07:10 +0200

    feat(request_reviews): allow to pick random user/teams
    This adds a new option `random_count` to the request_reviews action which
    allows to randomly select users and teams to request a review.
    
    Weight can be assigned to users and teams.
    
    The selected set of teams and users is always the same for a particular pull
    request.
    
    Related #1388

--- mergify_engine/tests/unit/actions/test_request_reviews.py
+++ mergify_engine/tests/unit/actions/test_request_reviews.py
@@ -11,9 +11,13 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
+from unittest import mock
+
 import pytest
 import voluptuous
 
+from mergify_engine import context
+from mergify_engine import subscription
 from mergify_engine.actions import request_reviews
 
 
@@ -62,3 +66,157 @@ def test_config_not_ok(config, error):
     with pytest.raises(voluptuous.MultipleInvalid) as p:
         request_reviews.RequestReviewsAction.get_schema()(config)
     assert str(p.value) == error
+
+
+def test_random_reviewers():
+    action = request_reviews.RequestReviewsAction.get_schema()(
+        {
+            "random_count": 2,
+            "teams": {
+                "foobar": 2,
+                "foobaz": 1,
+            },
+            "users": {
+                "jd": 2,
+                "sileht": 1,
+            },
+        },
+    )
+    reviewers = action._get_random_reviewers(123, "jd")
+    assert reviewers == {"@foobar", "sileht"}
+    reviewers = action._get_random_reviewers(124, "sileht")
+    assert reviewers == {"jd", "@foobar"}
+    reviewers = action._get_random_reviewers(124, "jd")
+    assert reviewers == {"@foobaz", "@foobar"}
+
+
+def test_random_reviewers_no_weight():
+    action = request_reviews.RequestReviewsAction.get_schema()(
+        {
+            "random_count": 2,
+            "teams": {
+                "foobar": 2,
+                "foobaz": 1,
+            },
+            "users": ["jd", "sileht"],
+        },
+    )
+    reviewers = action._get_random_reviewers(123, "another-jd")
+    assert reviewers == {"sileht", "jd"}
+    reviewers = action._get_random_reviewers(124, "another-jd")
+    assert reviewers == {"sileht", "@foobar"}
+    reviewers = action._get_random_reviewers(124, "sileht")
+    assert reviewers == {"@foobaz", "@foobar"}
+
+
+def test_random_reviewers_count_bigger():
+    action = request_reviews.RequestReviewsAction.get_schema()(
+        {
+            "random_count": 15,
+            "teams": {
+                "foobar": 2,
+                "foobaz": 1,
+            },
+            "users": {
+                "jd": 2,
+                "sileht": 45,
+            },
+        }
+    )
+    reviewers = action._get_random_reviewers(123, "foobar")
+    assert reviewers == {"@foobar", "@foobaz", "jd", "sileht"}
+    reviewers = action._get_random_reviewers(124, "another-jd")
+    assert reviewers == {"@foobar", "@foobaz", "jd", "sileht"}
+    reviewers = action._get_random_reviewers(124, "jd")
+    assert reviewers == {"@foobar", "@foobaz", "sileht"}
+
+
+def test_random_config_too_much_count():
+    with pytest.raises(voluptuous.MultipleInvalid) as p:
+        request_reviews.RequestReviewsAction.get_schema()(
+            {
+                "random_count": 20,
+                "teams": {
+                    "foobar": 2,
+                    "foobaz": 1,
+                },
+                "users": {
+                    "foobar": 2,
+                    "foobaz": 1,
+                },
+            },
+        )
+    assert (
+        str(p.value)
+        == "value must be at most 15 for dictionary value @ data['random_count']"
+    )
+
+
+def test_get_reviewers():
+    action = request_reviews.RequestReviewsAction.get_schema()(
+        {
+            "random_count": 2,
+            "teams": {
+                "foobar": 2,
+                "foobaz": 1,
+            },
+            "users": {
+                "jd": 2,
+                "sileht": 1,
+            },
+        },
+    )
+    reviewers = action._get_reviewers(843, set(), "another-jd")
+    assert reviewers == ({"jd", "sileht"}, set())
+    reviewers = action._get_reviewers(844, set(), "another-jd")
+    assert reviewers == ({"jd"}, {"foobar"})
+    reviewers = action._get_reviewers(845, set(), "another-jd")
+    assert reviewers == ({"sileht"}, {"foobar"})
+    reviewers = action._get_reviewers(845, {"sileht"}, "another-jd")
+    assert reviewers == (set(), {"foobar"})
+    reviewers = action._get_reviewers(845, {"jd"}, "another-jd")
+    assert reviewers == ({"sileht"}, {"foobar"})
+    reviewers = action._get_reviewers(845, set(), "SILEHT")
+    assert reviewers == ({"jd"}, {"foobar"})
+
+
+def test_disabled():
+    action = request_reviews.RequestReviewsAction.get_schema()(
+        {
+            "random_count": 2,
+            "teams": {
+                "foobar": 2,
+                "foobaz": 1,
+            },
+            "users": {
+                "jd": 2,
+                "sileht": 1,
+            },
+        },
+    )
+    client = mock.MagicMock()
+    client.auth.installation.__getitem__.return_value = 123
+    sub = subscription.Subscription(
+        123,
+        False,
+        "No sub",
+        {},
+        frozenset({}),
+    )
+    ctxt = context.Context(
+        client,
+        {
+            "number": 123,
+            "state": None,
+            "mergeable_state": "ok",
+            "merged_by": None,
+            "merged": None,
+            "merged_at": None,
+        },
+        sub,
+    )
+    assert action.run(ctxt, None, None) == (
+        "action_required",
+        "Random request reviews are disabled",
+        "⚠  The [subscription](https://dashboard.mergify.io/installation/123/subscription) needed to be updated to enable them.",
+    )
