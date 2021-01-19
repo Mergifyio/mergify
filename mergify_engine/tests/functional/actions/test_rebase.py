7f24ecc165b2fcaab7b64d33c8ca80c748f70ea6
CamClrt <camille.clarret@gmail.com>
2021-01-19 17:53:26 +0100

    test(actions): rebase action

---
+++ mergify_engine/tests/functional/actions/test_rebase.py
@@ -0,0 +1,52 @@
+# -*- encoding: utf-8 -*-
+#
+#  Copyright © 2021 Mergify SAS
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+import yaml
+
+from mergify_engine.tests.functional import base
+
+
+class TestRebaseAction(base.FunctionalTestBase):
+    async def test_rebase_ok(self):
+        rules = {
+            "pull_request_rules": [
+                {
+                    "name": "rebase",
+                    "conditions": [f"base={self.master_branch_name}"],
+                    "actions": {"rebase": {}},
+                }
+            ]
+        }
+
+        await self.setup_repo(yaml.dump(rules))
+
+        p, commits = await self.create_pr()
+        pr_initial_sha = commits[-1].sha
+
+        await self.git("checkout", self.master_branch_name)
+
+        open(self.git.tmp + "/an_other_file", "wb").close()
+        await self.git("add", "an_other_file")
+
+        await self.git("commit", "--no-edit", "-m", "commit ahead")
+        await self.git("push", "--quiet", "main", self.master_branch_name)
+        await self.wait_for("push", {"ref": f"refs/heads/{self.master_branch_name}"})
+
+        await self.run_engine(1)
+        p.update()
+
+        final_sha = p.head.sha
+
+        self.assertNotEqual(pr_initial_sha, final_sha)
