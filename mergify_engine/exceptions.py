7537edd5b93a70b0dc0bc479390f467d3d8312d6
Mehdi Abaakouk <sileht@sileht.net>
2021-02-02 19:58:39 +0100

    fix: never ever report redis connection issue in actions
    This will make the engine always retry after a redis connection issue
    instead of reporting failure to user.

--- mergify_engine/exceptions.py
+++ mergify_engine/exceptions.py
@@ -15,6 +15,8 @@ import dataclasses
 import datetime
 import typing
 
+import aredis
+
 from mergify_engine.clients import http
 
 
@@ -94,4 +96,8 @@ def need_retry(
         # correctly by mergify_engine.utils.Github()
         elif exception.response.status_code == 403:
             return datetime.timedelta(minutes=3)
+
+    elif isinstance(exception, aredis.exceptions.ConnectionError):
+        return datetime.timedelta(minutes=1)
+
     return None
