a34a2748b8b68e33db84c619f9ead2935f17509d
Mehdi Abaakouk <sileht@sileht.net>
2021-03-24 18:10:14 +0100

    fix: set count with using scan_iter

--- mergify_engine/web/root.py
+++ mergify_engine/web/root.py
@@ -310,7 +310,9 @@ async def queues(
     queues: typing.Dict[
         str, typing.Dict[str, typing.List[int]]
     ] = collections.defaultdict(dict)
-    async for queue in redis_cache.scan_iter(match=f"merge-*~{owner_id}~*"):
+    async for queue in redis_cache.scan_iter(
+        match=f"merge-*~{owner_id}~*", count=10000
+    ):
         queue_type, _, repo_id, branch = queue.split("~")
         if queue_type == "merge-queue":
             queues[repo_id][branch] = [
