423ed83395b3a1def90570faf39ea3028b995104
Mehdi Abaakouk <sileht@sileht.net>
2021-04-19 16:48:24 +0200

    fix(installer): set the default subscription url

--- installer/src/App.jsx
+++ installer/src/App.jsx
@@ -57,6 +57,7 @@ MERGIFYENGINE_OAUTH_CLIENT_SECRET=${respApp.data.client_secret}
 MERGIFYENGINE_WEBHOOK_SECRET=${respApp.data.webhook_secret}
 MERGIFYENGINE_PRIVATE_KEY=${Buffer.from(respApp.data.pem).toString('base64')}
 MERGIFYENGINE_CACHE_TOKEN_SECRET=${getRandomString(42)}
+MERGIFYENGINE_SUBSCRIPTION_BASE_URL="https://dashboard.mergify.io"
 PYTHONUNBUFFERED=1
 `);
     };
