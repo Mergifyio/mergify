824e4cf8acacf96db9897776b5c59539795be1ce
Mehdi Abaakouk <sileht@sileht.net>
2021-05-15 10:21:49 +0200

    feat(queue): track when the pull request gets queued
    Fixes MRGFY-453

--- mergify_engine/tests/unit/test_json.py
+++ mergify_engine/tests/unit/test_json.py
@@ -13,6 +13,7 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
+import datetime
 import enum
 import json
 
@@ -27,17 +28,29 @@ class Color(enum.Enum):
     BLUE = 3
 
 
-with_enum = {
+payload_decoded = {
     "name": "hello",
     "conditions": [],
     "actions": {"merge": {"strict": Color.BLUE}},
+    "datetime_naive": datetime.datetime(2021, 5, 15, 8, 35, 36, 442306),
+    "datetime_aware": datetime.datetime(
+        2021, 5, 15, 8, 41, 36, 796485, tzinfo=datetime.timezone.utc
+    ),
 }
 
-with_enum_encode = {
+payload_encoded = {
     "name": "hello",
     "conditions": [],
     "actions": {
         "merge": {"strict": {"__pytype__": "enum", "class": "Color", "name": "BLUE"}}
+    },
+    "datetime_naive": {
+        "__pytype__": "datetime.datetime",
+        "value": "2021-05-15T08:35:36.442306",
+    },
+    "datetime_aware": {
+        "__pytype__": "datetime.datetime",
+        "value": "2021-05-15T08:41:36.796485+00:00",
     },
 }
 
@@ -49,10 +62,10 @@ def test_register_type_fail() -> None:
         mergify_json.register_type(Color)
 
 
-def test_encode_enum() -> None:
-    assert json.loads(mergify_json.dumps(with_enum)) == with_enum_encode
+def test_encode() -> None:
+    assert json.loads(mergify_json.dumps(payload_decoded)) == payload_encoded
 
 
-def test_decode_enum() -> None:
-    json_file = mergify_json.dumps(with_enum)
-    assert mergify_json.loads(json_file) == with_enum
+def test_decode() -> None:
+    json_file = json.dumps(payload_encoded)
+    assert mergify_json.loads(json_file) == payload_decoded
--- mergify_engine/utils.py
+++ mergify_engine/utils.py
@@ -109,6 +109,40 @@ def utcnow():
     return datetime.datetime.now(tz=datetime.timezone.utc)
 
 
+def number_with_unit(n: int, unit: str) -> str:
+    if n >= -1 and n <= 1:
+        return f"{n} {unit}"
+    return f"{n} {unit}s"
+
+
+def pretty_join(strings: typing.List[str]) -> str:
+    if len(strings) == 0:
+        return ""
+    elif len(strings) == 1:
+        return strings[0]
+    else:
+        return f"{', '.join(strings[:-1])} and {strings[-1]}"
+
+
+def pretty_timedelta(t: datetime.timedelta) -> str:
+    seconds = int(t.total_seconds())
+    days, seconds = divmod(seconds, 86400)
+    hours, seconds = divmod(seconds, 3600)
+    minutes, seconds = divmod(seconds, 60)
+    parts = []
+    if days > 0:
+        parts.append(number_with_unit(days, "day"))
+    if days > 0 or hours > 0:
+        parts.append(number_with_unit(hours, "hour"))
+    if days > 0 or hours > 0 or minutes > 0:
+        parts.append(number_with_unit(minutes, "minute"))
+
+    if days == 0 and hours == 0 and minutes == 0:
+        parts.append(number_with_unit(seconds, "second"))
+
+    return pretty_join(parts)
+
+
 def unicode_truncate(s, length, encoding="utf-8"):
     """Truncate a string to length in bytes.
 
