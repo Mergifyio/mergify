e4b701ade6fa1ee35cb2d35e9d99614c92f2bab0
Erwan SIMONETTI <erwan.simonetti@epitech.eu>
2021-08-25 11:35:58 +0200

    feat(redis): track active users
    This changes store active users in redis and provides two ways to get
    them:
    * /organization/<owner_id>/usage API for SAAS dashboard
    * mergify-count-seats cli for on-premise
    
    Change-Id: I451b427c8bb9d2396deefdf63b8192e2e22e5aa9

--- mergify_engine/tests/functional/base.py
+++ mergify_engine/tests/functional/base.py
@@ -492,9 +492,13 @@ class FunctionalTestBase(unittest.IsolatedAsyncioTestCase):
         if RECORD:
             assert self.client_admin.auth.owner == "mergify-test1"
             assert self.client_fork.auth.owner == "mergify-test2"
+            assert self.client_admin.auth.owner_id == 38494943
+            assert self.client_fork.auth.owner_id == 38495008
         else:
             self.client_admin.auth.owner = github_types.GitHubLogin("mergify-test1")
             self.client_fork.auth.owner = github_types.GitHubLogin("mergify-test2")
+            self.client_admin.auth.owner_id = github_types.GitHubAccountIdType(38494943)
+            self.client_fork.auth.owner_id = github_types.GitHubAccountIdType(38495008)
 
         self.url_origin = (
             f"/repos/mergifyio-testing/{self.RECORD_CONFIG['repository_name']}"
