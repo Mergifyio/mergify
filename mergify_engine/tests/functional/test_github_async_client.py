ErwanSimonetti/mergify-engine

fix(github): using exclusively owner ID and not login for GitHub auth

mergify_engine/tests/functional/test_github_async_client.py
@@ -16,6 +16,7 @@
 
 import yaml
 
+from mergify_engine import debug
 from mergify_engine.clients import github
 from mergify_engine.clients import http
 from mergify_engine.tests.functional import base
@@ -39,7 +40,8 @@ class TestGithubClient(base.FunctionalTestBase):
         p2, _ = await self.create_pr()
         await self.create_pr(base=other_branch)
 
-        client = github.aget_client("mergifyio-testing")
+        owner_id = await debug.get_owner_id_from_login("mergifyio-testing")
+        client = github.aget_client(owner_id)
 
         url = f"/repos/mergifyio-testing/{self.RECORD_CONFIG['repository_name']}/pulls"
 

mergify_engine/web/root.py
@@ -29,6 +29,7 @@ import voluptuous
 from mergify_engine import check_api  # noqa
 from mergify_engine import config
 from mergify_engine import count_seats
+from mergify_engine import debug
 from mergify_engine import github_events
 from mergify_engine import github_types
 from mergify_engine import json
@@ -91,7 +92,11 @@ async def refresh_repo(
         redis.get_redis_stream
     ),
 ) -> responses.Response:
-    async with github.aget_client(owner_name=owner) as client:
+    async with github.aget_client(
+        owner_id=github_types.GitHubAccountIdType(
+            await debug.get_owner_id_from_login(owner)
+        )
+    ) as client:
         try:
             repository = await client.item(f"/repos/{owner}/{repo_name}")
         except http.HTTPNotFound:
@@ -125,7 +130,11 @@ async def refresh_pull(
     ),
 ) -> responses.Response:
     action = RefreshActionSchema(action)
-    async with github.aget_client(owner_name=owner) as client:
+    async with github.aget_client(
+        owner_id=github_types.GitHubAccountIdType(
+            await debug.get_owner_id_from_login(owner)
+        )
+    ) as client:
         try:
             repository = await client.item(f"/repos/{owner}/{repo_name}")
         except http.HTTPNotFound:
@@ -159,7 +168,11 @@ async def refresh_branch(
         redis.get_redis_stream
     ),
 ) -> responses.Response:
-    async with github.aget_client(owner_name=owner) as client:
+    async with github.aget_client(
+        owner_id=github_types.GitHubAccountIdType(
+            await debug.get_owner_id_from_login(owner)
+        )
+    ) as client:
         try:
             repository = await client.item(f"/repos/{owner}/{repo_name}")
         except http.HTTPNotFound:

mergify_engine/web/simulator.py
@@ -24,6 +24,7 @@ from starlette.middleware import cors
 import voluptuous
 
 from mergify_engine import context
+from mergify_engine import debug
 from mergify_engine import exceptions
 from mergify_engine import github_types
 from mergify_engine import rules
@@ -117,7 +118,8 @@ async def _simulator(
         if token:
             auth = github.GithubTokenAuth(token)
         else:
-            auth = github.get_auth(owner)
+            owner_id = await debug.get_owner_id_from_login(owner)
+            auth = github.get_auth(github_types.GitHubAccountIdType(owner_id))
 
         async with github.aget_client(auth=auth) as client:
             try:

Mergifyio/mergify

mergify_engine/tests/functional/test_github_async_client.py
@@ -39,7 +39,8 @@ class TestGithubClient(base.FunctionalTestBase):
         p2, _ = await self.create_pr()
         await self.create_pr(base=other_branch)
 
-        client = github.aget_client("mergifyio-testing")
+        owner_id = await github.get_owner_id_from_login("mergifyio-testing")
+        client = github.aget_client(owner_id)
 
         url = f"/repos/mergifyio-testing/{self.RECORD_CONFIG['repository_name']}/pulls"
 

mergify_engine/web/root.py
@@ -91,7 +91,11 @@ async def refresh_repo(
         redis.get_redis_stream
     ),
 ) -> responses.Response:
-    async with github.aget_client(owner_name=owner) as client:
+    async with github.aget_client(
+        owner_id=github_types.GitHubAccountIdType(
+            await github.get_owner_id_from_login(owner)
+        )
+    ) as client:
         try:
             repository = await client.item(f"/repos/{owner}/{repo_name}")
         except http.HTTPNotFound:
@@ -125,7 +129,11 @@ async def refresh_pull(
     ),
 ) -> responses.Response:
     action = RefreshActionSchema(action)
-    async with github.aget_client(owner_name=owner) as client:
+    async with github.aget_client(
+        owner_id=github_types.GitHubAccountIdType(
+            await github.get_owner_id_from_login(owner)
+        )
+    ) as client:
         try:
             repository = await client.item(f"/repos/{owner}/{repo_name}")
         except http.HTTPNotFound:
@@ -159,7 +167,11 @@ async def refresh_branch(
         redis.get_redis_stream
     ),
 ) -> responses.Response:
-    async with github.aget_client(owner_name=owner) as client:
+    async with github.aget_client(
+        owner_id=github_types.GitHubAccountIdType(
+            await github.get_owner_id_from_login(owner)
+        )
+    ) as client:
         try:
             repository = await client.item(f"/repos/{owner}/{repo_name}")
         except http.HTTPNotFound:

mergify_engine/web/simulator.py
@@ -117,7 +117,8 @@ async def _simulator(
         if token:
             auth = github.GithubTokenAuth(token)
         else:
-            auth = github.get_auth(owner)
+            owner_id = await github.get_owner_id_from_login(owner)
+            auth = github.get_auth(github_types.GitHubAccountIdType(owner_id))
 
         async with github.aget_client(auth=auth) as client:
             try: