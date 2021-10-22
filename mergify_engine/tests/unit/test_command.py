8f16e1b2fe32fb90ef481fbd2b401359c9aacdb5
Mehdi Abaakouk <sileht@sileht.net>
2021-10-22 09:13:19 +0200

    chore(typing): cover unit/test_command.py
    Change-Id: I29b102d8d43b5da6b2fc1e5b08b298716a5850af

--- mergify_engine/tests/unit/test_command.py
+++ mergify_engine/tests/unit/test_command.py
@@ -20,22 +20,35 @@ import pytest
 from mergify_engine import config
 from mergify_engine import context
 from mergify_engine import github_types
+from mergify_engine import rules
+from mergify_engine import utils
 from mergify_engine.actions.backport import BackportAction
 from mergify_engine.actions.rebase import RebaseAction
+from mergify_engine.clients import github
 from mergify_engine.dashboard import subscription
 from mergify_engine.engine import commands_runner
 
 
-def test_command_loader():
-    config = {"raw": {}}
+EMPTY_CONFIG = rules.get_mergify_config(
+    context.MergifyConfigFile(
+        type="file",
+        content="whatever",
+        sha=github_types.SHAType("azertyuiop"),
+        path="whatever",
+        decoded_content=b"",
+    )
+)
+
+
+def test_command_loader() -> None:
     with pytest.raises(commands_runner.CommandInvalid):
-        action = commands_runner.load_command(config, "@mergifyio notexist foobar\n")
+        commands_runner.load_command(EMPTY_CONFIG, "@mergifyio notexist foobar\n")
 
     with pytest.raises(commands_runner.CommandInvalid):
-        action = commands_runner.load_command(config, "@mergifyio comment foobar\n")
+        commands_runner.load_command(EMPTY_CONFIG, "@mergifyio comment foobar\n")
 
     with pytest.raises(commands_runner.CommandInvalid):
-        action = commands_runner.load_command(config, "@Mergifyio comment foobar\n")
+        commands_runner.load_command(EMPTY_CONFIG, "@Mergifyio comment foobar\n")
 
     for message in [
         "@mergify rebase",
@@ -45,12 +58,12 @@ def test_command_loader():
         "@mergifyio rebase foobar",
         "@mergifyio rebase foobar\nsecondline\n",
     ]:
-        command, args, action = commands_runner.load_command(config, message)
+        command, args, action = commands_runner.load_command(EMPTY_CONFIG, message)
         assert command == "rebase"
         assert isinstance(action, RebaseAction)
 
     command, args, action = commands_runner.load_command(
-        config, "@mergifyio backport branch-3.1 branch-3.2\nfoobar\n"
+        EMPTY_CONFIG, "@mergifyio backport branch-3.1 branch-3.2\nfoobar\n"
     )
     assert command == "backport"
     assert args == "branch-3.1 branch-3.2"
@@ -68,19 +81,25 @@ def test_command_loader():
     }
 
 
-def test_command_loader_with_defaults():
-    config = {
-        "raw": {
-            "defaults": {
-                "actions": {
-                    "backport": {
-                        "branches": ["branch-3.1", "branch-3.2"],
-                        "ignore_conflicts": False,
-                    }
-                }
-            }
-        }
-    }
+def test_command_loader_with_defaults() -> None:
+    raw_config = """
+defaults:
+  actions:
+    backport:
+      branches:
+        - branch-3.1
+        - branch-3.2
+      ignore_conflicts: false
+"""
+
+    file = context.MergifyConfigFile(
+        type="file",
+        content="whatever",
+        sha=github_types.SHAType("azertyuiop"),
+        path="whatever",
+        decoded_content=raw_config.encode(),
+    )
+    config = rules.get_mergify_config(file)
     command = commands_runner.load_command(config, "@mergifyio backport")
     assert command.name == "backport"
     assert command.args == ""
@@ -98,64 +117,112 @@ def test_command_loader_with_defaults():
     }
 
 
-async def _create_context(redis_cache, client):
+async def _create_context(
+    redis_cache: utils.RedisCache, client: github.AsyncGithubInstallationClient
+) -> context.Context:
     sub = subscription.Subscription(
         redis_cache,
         123,
         "",
-        {},
         frozenset({subscription.Features.PUBLIC_REPOSITORY}),
+        0,
+    )
+    gh_user = github_types.GitHubAccount(
+        {
+            "type": "User",
+            "id": github_types.GitHubAccountIdType(1),
+            "login": github_types.GitHubLogin("octocat"),
+            "avatar_url": "",
+        }
     )
 
-    installation = context.Installation(123, "Mergifyio", sub, client, redis_cache)
-
-    repository = context.Repository(
-        installation, {"name": "demo", "id": 123, "private": True}
+    gh_owner = github_types.GitHubAccount(
+        {
+            "type": "User",
+            "id": github_types.GitHubAccountIdType(123),
+            "login": github_types.GitHubLogin("Mergifyio"),
+            "avatar_url": "",
+        }
     )
+    gh_repo = github_types.GitHubRepository(
+        {
+            "archived": False,
+            "url": "",
+            "html_url": "",
+            "default_branch": github_types.GitHubRefType("main"),
+            "id": github_types.GitHubRepositoryIdType(123),
+            "full_name": "user/ref",
+            "name": github_types.GitHubRepositoryName("demo"),
+            "private": False,
+            "owner": gh_owner,
+        }
+    )
+
+    installation = context.Installation(
+        github_types.GitHubAccountIdType(123),
+        github_types.GitHubLogin("Mergifyio"),
+        sub,
+        client,
+        redis_cache,
+    )
+
+    repository = context.Repository(installation, gh_repo)
 
     return await context.Context.create(
         repository,
-        {
-            "number": 789,
-            "state": "open",
-            "title": "Amazing new feature",
-            "user": {
-                "login": "octocat",
-                "id": 1,
-            },
-            "mergeable_state": "ok",
-            "merged_by": None,
-            "merged": None,
-            "merged_at": None,
-            "base": {
-                "sha": "sha",
-                "ref": "main",
-                "user": {
-                    "login": {
-                        "Mergifyio",
-                    },
+        github_types.GitHubPullRequest(
+            {
+                "node_id": "42",
+                "locked": False,
+                "assignees": [],
+                "requested_reviewers": [],
+                "requested_teams": [],
+                "milestone": None,
+                "title": "",
+                "body": "",
+                "updated_at": github_types.ISODateTimeType("2021-06-01T18:41:39Z"),
+                "created_at": github_types.ISODateTimeType("2021-06-01T18:41:39Z"),
+                "closed_at": None,
+                "id": github_types.GitHubPullRequestId(0),
+                "maintainer_can_modify": False,
+                "rebaseable": False,
+                "draft": False,
+                "merge_commit_sha": None,
+                "labels": [],
+                "number": github_types.GitHubPullRequestNumber(789),
+                "commits": 1,
+                "state": "open",
+                "html_url": "<html_url>",
+                "user": gh_user,
+                "merged": False,
+                "head": {
+                    "label": "Mergifyio:feature",
+                    "sha": github_types.SHAType("sha2"),
+                    "ref": github_types.GitHubRefType("feature"),
+                    "user": gh_owner,
+                    "repo": gh_repo,
                 },
-                "repo": {
-                    "name": "demo",
-                    "private": False,
-                    "owner": {
-                        "login": "Mergifyio",
-                        "id": 123,
-                    },
-                    "permissions": {
-                        "admin": False,
-                        "push": False,
-                        "pull": True,
-                    },
+                "base": {
+                    "label": "Mergifyio:main",
+                    "sha": github_types.SHAType("sha1"),
+                    "ref": github_types.GitHubRefType("main"),
+                    "user": gh_owner,
+                    "repo": gh_repo,
                 },
-            },
-        },
+                "merged_by": None,
+                "merged_at": None,
+                "mergeable_state": "clean",
+                "changed_files": 300,
+            }
+        ),
         [],
     )
 
 
 @pytest.mark.asyncio
-async def test_run_command_without_rerun_and_without_user(redis_cache):
+async def test_run_command_without_rerun_and_without_user(
+    redis_cache: utils.RedisCache,
+) -> None:
 
     client = mock.MagicMock()
     client.auth.installation.__getitem__.return_value = 123
@@ -164,13 +231,18 @@ async def test_run_command_without_rerun_and_without_user(redis_cache):
 
     with pytest.raises(RuntimeError) as error_msg:
         await commands_runner.handle(
-            ctxt=ctxt, mergify_config={}, comment="@Mergifyio update", user=None
+            ctxt=ctxt,
+            mergify_config=EMPTY_CONFIG,
+            comment="@Mergifyio update",
+            user=None,
         )
     assert "user must be set if rerun is false" in str(error_msg.value)
 
 
 @pytest.mark.asyncio
-async def test_run_command_with_rerun_and_without_user(redis_cache):
+async def test_run_command_with_rerun_and_without_user(
+    redis_cache: utils.RedisCache,
+) -> None:
 
     client = mock.Mock()
     client.post = mock.AsyncMock()
@@ -179,7 +251,7 @@ async def test_run_command_with_rerun_and_without_user(redis_cache):
 
     await commands_runner.handle(
         ctxt=ctxt,
-        mergify_config={},
+        mergify_config=EMPTY_CONFIG,
         comment="@mergifyio something",
         user=None,
         rerun=True,
@@ -227,7 +299,9 @@ async def test_run_command_with_rerun_and_without_user(redis_cache):
     ],
 )
 @pytest.mark.asyncio
-async def test_run_command_with_user(user_id, permission, result, redis_cache):
+async def test_run_command_with_user(
+    user_id: int, permission: str, result: str, redis_cache: utils.RedisCache
+) -> None:
 
     client = mock.Mock()
 
@@ -235,8 +309,8 @@ async def test_run_command_with_user(user_id, permission, result, redis_cache):
 
     user = github_types.GitHubAccount(
         {
-            "id": user_id,
-            "login": "wall-e",
+            "id": github_types.GitHubAccountIdType(user_id),
+            "login": github_types.GitHubLogin("wall-e"),
             "type": "Bot",
             "avatar_url": "https://avatars.githubusercontent.com/u/583231?v=4",
         },
@@ -245,15 +319,13 @@ async def test_run_command_with_user(user_id, permission, result, redis_cache):
     client.item = mock.AsyncMock()
     client.item.return_value = {
         "permission": permission,
-        "user": {
-            "login": "wall-e",
-        },
+        "user": user,
     }
     client.post = mock.AsyncMock()
 
     await commands_runner.handle(
         ctxt=ctxt,
-        mergify_config={},
+        mergify_config=EMPTY_CONFIG,
         comment="unrelated",
         user=None,
         rerun=True,
@@ -261,7 +333,10 @@ async def test_run_command_with_user(user_id, permission, result, redis_cache):
     assert len(client.post.call_args_list) == 0
 
     await commands_runner.handle(
-        ctxt=ctxt, mergify_config={}, comment="@mergifyio something", user=user
+        ctxt=ctxt,
+        mergify_config=EMPTY_CONFIG,
+        comment="@mergifyio something",
+        user=user,
     )
 
     assert len(client.post.call_args_list) == 1
@@ -269,7 +344,7 @@ async def test_run_command_with_user(user_id, permission, result, redis_cache):
 
 
 @pytest.mark.asyncio
-async def test_run_command_with_wrong_arg(redis_cache):
+async def test_run_command_with_wrong_arg(redis_cache: utils.RedisCache) -> None:
 
     client = mock.Mock()
     client.post = mock.AsyncMock()
@@ -278,7 +353,7 @@ async def test_run_command_with_wrong_arg(redis_cache):
 
     await commands_runner.handle(
         ctxt=ctxt,
-        mergify_config={"raw": {}},
+        mergify_config=EMPTY_CONFIG,
         comment="@mergifyio squash invalid-arg",
         rerun=True,
         user=None,
