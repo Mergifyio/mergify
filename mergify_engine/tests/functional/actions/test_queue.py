1b4af7a68af118e6868e3a872322913b2c237d9f
Mehdi Abaakouk <sileht@sileht.net>
2021-12-07 09:40:19 +0100

    chore(tests): test if empty queue conditions wait the CIs
    It doesn't ....
    
    Change-Id: I20320583fc3a00cc29cf98de69f35b670eba6208

--- mergify_engine/tests/functional/actions/test_queue.py
+++ mergify_engine/tests/functional/actions/test_queue.py
@@ -2624,6 +2624,85 @@ DO NOT EDIT
 
         await self._assert_cars_contents(q, None, [])
 
+    async def test_queue_without_queue_conditions(self):
+        rules = {
+            "queue_rules": [
+                {
+                    "name": "default",
+                    "conditions": [],
+                }
+            ],
+            "pull_request_rules": [
+                {
+                    "name": "Merge priority high",
+                    "conditions": [
+                        f"base={self.main_branch_name}",
+                        "check-success=continuous-integration/fake-ci",
+                    ],
+                    "actions": {
+                        "queue": {
+                            "method": "squash",
+                            "name": "default",
+                        }
+                    },
+                },
+            ],
+        }
+        await self.setup_repo(yaml.dump(rules))
+
+        p1, _ = await self.create_pr()
+
+        # To force others to be rebased
+        p, _ = await self.create_pr()
+        await self.merge_pull(p["number"])
+        await self.wait_for("pull_request", {"action": "closed"})
+        await self.run_engine()
+        p = await self.get_pull(p["number"])
+
+        await self.create_status(p1)
+        await self.run_engine()
+
+        await self.wait_for("pull_request", {"action": "synchronize"})
+        await self.run_engine()
+
+        ctxt = context.Context(self.repository_ctxt, p)
+        q = await merge_train.Train.from_context(ctxt)
+        await self._assert_cars_contents(
+            q,
+            p["merge_commit_sha"],
+            [
+                TrainCarMatcher(
+                    [p1["number"]],
+                    [],
+                    p["merge_commit_sha"],
+                    "updated",
+                    None,
+                ),
+            ],
+        )
+
+        head_sha = p1["head"]["sha"]
+        p1 = await self.get_pull(p1["number"])
+        assert p1["head"]["sha"] != head_sha  # ensure it have been rebased
+
+        check = first(
+            await context.Context(self.repository_ctxt, p1).pull_engine_check_runs,
+            key=lambda c: c["name"] == "Rule: Merge priority high (queue)",
+        )
+        assert (
+            check["output"]["title"]
+            == "The pull request is the 1st in the queue to be merged"
+        )
+
+        await self.create_status(p1)
+        await self.run_engine()
+        await self.wait_for("pull_request", {"action": "closed"})
+
+        pulls = await self.get_pulls()
+        assert len(pulls) == 0
+
+        await self._assert_cars_contents(q, None, [])
+
     async def test_queue_without_branch_protection_for_queueing(self):
         rules = {
             "queue_rules": [
