44f69908ceabcf2f1d5b1204ebed5ddc285c4ce2
Mehdi ABAAKOUK <sileht@sileht.net>
2021-12-07 17:21:27 +0100

    feat(context): cache user/teams permissions v2 (#3719)
    This change uses the same technique as other cached thing.
    
    Fixes MRGFY-750
    
    Change-Id: Idc27c44614edf6cfcda8109a54f6c73bf10d9ed8

---
+++ mergify_engine/cache.py
@@ -0,0 +1,61 @@
+#
+# Copyright © 2021 Mergify SAS
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+import dataclasses
+import enum
+import typing
+
+
+_K = typing.TypeVar("_K")
+_V = typing.TypeVar("_V")
+
+
+# NOTE(sileht): Sentinel object (eg: `marker = object()`) can't be expressed
+# with typing yet use the proposed workaround instead:
+#   https://github.com/python/typing/issues/689
+#   https://www.python.org/dev/peps/pep-0661/
+class UnsetType(enum.Enum):
+    _MARKER = 0
+
+
+Unset: typing.Final = UnsetType._MARKER
+
+
+@dataclasses.dataclass
+class Cache(typing.Generic[_K, _V]):
+    _cache: typing.Dict[_K, _V] = dataclasses.field(default_factory=dict)
+
+    def get(self, key: _K) -> typing.Union[_V, UnsetType]:
+        return self._cache.get(key, Unset)
+
+    def set(self, key: _K, value: _V) -> None:
+        self._cache[key] = value
+
+    def delete(self, key: _K) -> None:
+        try:
+            del self._cache[key]
+        except KeyError:
+            pass
+
+    def clear(self) -> None:
+        self._cache.clear()
+
+    def __setitem__(self, key: _K, value: _V) -> None:
+        return self.set(key, value)
+
+    def __getitem__(self, key: _K) -> typing.Union[_V, UnsetType]:
+        return self.get(key)
+
+    def __delitem__(self, key: _K) -> None:
+        self.delete(key)
