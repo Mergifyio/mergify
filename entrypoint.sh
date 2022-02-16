c799531f87a6f8766c9f7174bd9d5e3ab26aa371
Mehdi Abaakouk <sileht@sileht.net>
2022-02-16 14:43:17 +0100

    ci(semgrep): escape some bash variables
    Change-Id: I2768ba9ea0d6497db115b454077fa224c9410da0

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,11 @@ get_command() {
     sed -n -e "s/^$1://p" Procfile
 }
 
-MODE=${1:-aio}
+MODE="${1:-aio}"
 
 if [ "$MERGIFYENGINE_INTEGRATION_ID" ]; then
-  case ${MODE} in
-      web|worker) exec $(get_command $1);;
+  case "${MODE}" in
+      web|worker) exec $(get_command "$1");;
       aio) exec honcho start;;
       *) echo "usage: $0 (web|worker|aio)";;
   esac
