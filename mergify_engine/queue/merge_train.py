4693711871ca4cb584a72fafce81688c5cd80f4e
Mehdi Abaakouk <sileht@sileht.net>
2022-04-10 08:17:17 +0200

    fix(queue): automatically cleanup left over pull requests
    Fixes MRGFY-1063
    
    Change-Id: I39c5727eff685960adb2b2da37534781862a0f2f

--- mergify_engine/queue/merge_train.py
+++ mergify_engine/queue/merge_train.py
@@ -1355,6 +1355,7 @@ class Train(queue.QueueBase):
         await self._sync_configuration_change(queue_rules)
         await self._split_failed_batches(queue_rules)
         await self._populate_cars(queue_rules)
+        # FIXME(sileht): we should not ran that on each worker run, once a hour or once a day should be enough
         await self._clean_unsused_merge_queue_branches()
         await self.save()
 
@@ -1380,6 +1381,22 @@ class Train(queue.QueueBase):
             if branch_ref not in list_car_branch_refs:
                 self.log.info("removing orphan merge-queue branch", branch=branch_ref)
                 await self._delete_branch(branch_ref)
+
+        # NOTE(sileht): This is time consuming and slow... but we don't have much choose
+        # We should run this method less often.
+        async for pull in self.repository.installation.client.items(
+            f"/repos/{self.repository.installation.owner_login}/{self.repository.repo['name']}/pulls",
+            params={"base": self.ref},
+        ):
+            if (
+                pull["base"]["ref"] == self.ref
+                and pull["head"]["repo"]["id"] == self.repository.repo["id"]
+                and pull["head"]["ref"].startswith(
+                    f"{constants.MERGE_QUEUE_BRANCH_PREFIX}/"
+                )
+                and pull["head"]["ref"] not in list_car_branch_refs
+            ):
+                await self._close_pull_request(pull["number"])
 
     async def _remove_duplicate_pulls(self) -> None:
         known_prs = set()
